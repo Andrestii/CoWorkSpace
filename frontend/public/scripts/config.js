77e3e5bf63213325d77fe22fb63604d648e14df0
Stefano Momi <stefano.momi@outlook.com>
2025-08-10 13:21:11 +0200

    aggiunto pag principale

--- frontend/public/scripts/config.js
+++ frontend/public/scripts/config.js
@@ -1,1 +1,16 @@
-// Configurazione globale del progetto
+const config = {
+    development: {
+        apiUrl: "http://localhost:3000/api", // url dev
+    },
+    production: {
+        apiUrl: "https://coworkspace.onrender.com/api", // URL di produzione
+    },
+};
+
+const isProduction =
+    window.location.hostname !== "localhost" &&
+    !window.location.hostname.includes("127.0.0.1");
+
+// Esporta la configurazione corretta
+const apiConfig = isProduction ? config.production : config.development;
+console.log("API base:", apiConfig.apiUrl);
